e5b91ada8329d4462975a9e76c03dac7b6cd3dc9
Adam <adam.lj.haglund@gmail.com>
2019-05-26 14:44:12 +0200

    update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,11 +27,22 @@ module.exports = {
   ],
   rules: {
     'node/no-unsupported-features/es-syntax': 0,
+    'node/prefer-promises/dns': 2,
+    'node/prefer-promises/fs': 2,
     'import/no-default-export': 2,
+    'import/no-useless-path-segments': [
+      2,
+      {
+        noUselessIndex: true,
+      },
+    ],
     '@typescript-eslint/explicit-function-return-type': 0,
     '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/no-non-null-assertion': 0,
     '@typescript-eslint/interface-name-prefix': [2, 'always'],
+    '@typescript-eslint/prefer-includes': 2,
+    '@typescript-eslint/prefer-string-starts-ends-with': 2,
+    '@typescript-eslint/await-thenable': 2,
     'prettier/prettier': 0,
   },
   settings: {
